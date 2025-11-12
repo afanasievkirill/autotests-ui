18d5314f44fffa8af2e463c3fe7937ae3985e5b8
afanasievkirill <afanasiev.kirill@gmail.com>
2025-11-12 13:38:01 +0300

    5.6.10

---
+++ playwright_courses.py
@@ -0,0 +1,55 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
+    context = browser.new_context()
+    # Открываем новую страницу в рамках контекста
+    page = context.new_page()
+
+    # Переходим на страницу входа
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    # Заполняем поле email
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
+
+    # Заполняем поле юзернэйм
+    user_name_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    user_name_input.fill('username')
+
+    # Заполняем поле пароль
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill("password")
+
+    # Нажимаем на кнопку Login
+    login_button = page.get_by_test_id('registration-page-registration-button')
+    login_button.click()
+
+    dashboard_toolbar = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_toolbar).to_be_visible()
+    expect(dashboard_toolbar).to_have_text('Dashboard')
+
+    context.storage_state(path="browser-state.json")
+
+    # вынес контекст в отдельную переменную так как тест может работать с двумя  вкладками
+    new_context = browser.new_context(storage_state="browser-state.json")
+    new_page = context.new_page()
+    new_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+    cources_toolbar = new_page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(cources_toolbar).to_be_visible()
+    expect(cources_toolbar).to_have_text('Courses')
+
+    cources_empty_icon = new_page.get_by_test_id('courses-list-empty-view-icon')
+    expect(cources_empty_icon).to_be_visible()
+
+    courses_title_text_empty = new_page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(courses_title_text_empty).to_be_visible()
+    expect(courses_title_text_empty).to_have_text('There is no results')
+
+    courses_list_description_text = new_page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(courses_list_description_text).to_be_visible()
+    expect(courses_list_description_text).to_have_text('Results from the load test pipeline will be displayed here')
+
+    
--- playwright_registration.py
+++ playwright_registration.py
@@ -1,31 +1,27 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
+# Открываем браузер с использованием Playwright
 with sync_playwright() as playwright:
-    # Открываем браузер и создаем новую страницу
+    # Запускаем Chromium браузер в обычном режиме (не headless)
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
+    context = browser.new_context()
+    # Открываем новую страницу в рамках контекста
+    page = context.new_page()
 
-    # Переходим на страницу входа
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    # Заполняем поле email
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
+    email_input.fill('user.name@gmail.com')
 
-    # Заполняем поле юзернэйм
-    user_name_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    user_name_input.fill('username')
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
 
-    # Заполняем поле пароль
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill("password")
+    password_input.fill('password')
 
-    # Нажимаем на кнопку Login
-    login_button = page.get_by_test_id('registration-page-registration-button')
-    login_button.click()
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
 
-    dashboard = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard).to_be_visible()
-    expect(dashboard).to_have_text('Dashboard')
-
-    page.wait_for_timeout(5000)
+    # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
+    context.storage_state(path="browser-state.json")
