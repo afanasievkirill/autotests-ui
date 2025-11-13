7d95926c71d2a3e5b1607cb6540ad320292ba39d
afanasievkirill <afanasiev.kirill@gmail.com>
2025-11-13 10:43:10 +0300

    6.7.14

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -15,24 +15,18 @@ def initialize_browser_state(playwright: Playwright):
     context = browser.new_context()
     page = context.new_page()
 
-    page.goto(
-        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration"
-    )
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    email_input = page.get_by_test_id("registration-form-email-input").locator("input")
-    email_input.fill("user.name@gmail.com")
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
 
-    username_input = page.get_by_test_id("registration-form-username-input").locator(
-        "input"
-    )
-    username_input.fill("username")
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
 
-    password_input = page.get_by_test_id("registration-form-password-input").locator(
-        "input"
-    )
-    password_input.fill("password")
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
 
-    registration_button = page.get_by_test_id("registration-page-registration-button")
+    registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
     context.storage_state(path="browser-state.json")
